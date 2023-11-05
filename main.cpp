d7ef08d6a57b8a26829c10e5fce095bcc1ca5b80
MaksimMyson <maksimmyson33@gmail.com>
2023-11-05 11:05:08 +0200

    ex2

--- main.cpp
+++ main.cpp
@@ -2,8 +2,25 @@
 
 using namespace std;
 
-int main()
-{
-	
+void determineSign(int* number) {
+	if (*number > 0) {
+		cout << "Positive" << endl;
+	}
+	else if (*number < 0) {
+		cout << "Negative" << endl;
+	}
+	else {
+		cout << "Zero" << endl;
+	}
+}
+
+int main() {
+	int num;
+
+	cout << "Enter a number: ";
+	cin >> num;
+
+	determineSign(&num);
+
 	return 0;
 }

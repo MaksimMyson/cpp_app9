cad8378853ea2cb0a46922a4e32011c0f963a3a6
MaksimMyson <maksimmyson33@gmail.com>
2023-11-05 11:42:17 +0200

    ex5

--- main.cpp
+++ main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
+#include <cstdlib> 
 
-using namespace std;
+int main() {
+    using namespace std;
 
-int main()
-{
-	
-	return 0;
+    const int arraySize = 5;
+    int numbers[arraySize];
+
+    for (int i = 0; i < arraySize; i++) {
+        numbers[i] = rand() % 100;  
+    }
+
+    int sum = 0;
+
+    int* ptr = numbers;
+
+    cout << "Array elements: ";
+    for (int i = 0; i < arraySize; i++) {
+        cout << numbers[i] << " ";
+        sum += *ptr;
+        ptr++;
+    }
+    cout << endl;
+
+    cout << "Sum of elements in the array: " << sum << endl;
+
+    return 0;
 }
+
+
+

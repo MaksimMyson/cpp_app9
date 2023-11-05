682926ce1565d31fefa63b6a3792b602b37e2183
MaksimMyson <maksimmyson33@gmail.com>
2023-11-05 10:56:45 +0200

    ex1

--- main.cpp
+++ main.cpp
@@ -2,8 +2,26 @@
 
 using namespace std;
 
-int main()
-{
-	
-	return 0;
+int findMax(int* num1, int* num2) {
+    if (*num1 > *num2) {
+        return *num1;
+    }
+    else {
+        return *num2;
+    }
+}
+
+int main() {
+    int num1, num2;
+
+    cout << "Enter the first number: ";
+    cin >> num1;
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    int result = findMax(&num1, &num2);
+
+    cout << "The maximum number between " << num1 << " and " << num2 << " is: " << result << std::endl;
+
+    return 0;
 }

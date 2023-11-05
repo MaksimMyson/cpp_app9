4e93d4ce2d2518d8efc65dbf34dd76cd93804b3a
MaksimMyson <maksimmyson33@gmail.com>
2023-11-05 11:17:50 +0200

    ex3

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-	
-	return 0;
+void swapValues(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int num1, num2;
+
+    cout << "Enter the first number: ";
+    cin >> num1;
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    cout << "Before swapping: num1 = " << num1 << ", num2 = " << num2 << std::endl;
+
+    swapValues(&num1, &num2);
+
+    cout << "After swapping: num1 = " << num1 << ", num2 = " << num2 << std::endl;
+
+    return 0;
 }

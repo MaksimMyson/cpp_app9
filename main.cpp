b9b2737be8b3315776f9c02a636f113555d4e0f2
MaksimMyson <maksimmyson33@gmail.com>
2023-11-06 15:21:23 +0200

    ex2

--- main.cpp
+++ main.cpp
@@ -2,8 +2,34 @@
 
 using namespace std;
 
-int main()
-{
-	
-	return 0;
+int main() {
+    const int size = 5;
+
+    int arr[size] = { 1, 2, 3, 4, 5 };
+
+    cout << "Original Array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    int* start = arr;
+    int* end = arr + size - 1;
+
+    while (start < end) {
+        int temp = *start;
+        *start = *end;
+        *end = temp;
+
+        start++;
+        end--;
+    }
+
+    cout << "Reversed Array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }

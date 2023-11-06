8e58d8f1087793083268d553bcd008c2e8a8db23
MaksimMyson <maksimmyson33@gmail.com>
2023-11-06 14:40:41 +0200

    ex1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
-int main()
-{
-	
-	return 0;
+int main() {
+    const int size = 5;
+
+    int sourceArray[size];
+    int destinationArray[size];
+
+    srand(static_cast<unsigned>(time(0)));
+    for (int i = 0; i < size; i++) {
+        sourceArray[i] = rand() % 100;
+    }
+
+    cout << "Source Array: ";
+    for (int i = 0; i < size; i++) {
+        cout << sourceArray[i] << " ";
+    }
+    cout << endl;
+
+    int* sourcePtr = sourceArray;
+    int* destinationPtr = destinationArray;
+
+    for (int i = 0; i < size; i++) {
+        *destinationPtr = *sourcePtr;
+        sourcePtr++;
+        destinationPtr++;
+    }
+
+    cout << "Destination Array: ";
+    for (int i = 0; i < size; i++) {
+        cout << destinationArray[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
